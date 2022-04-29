bd2238355e2a66eaa2d80d8aea005ce625070159
Dickkson <jarongud@gmail.com>
2022-04-28 19:18:43 -0700

    updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@
  *  * _strlen_recursion - returns the length of a string.
  *   * @s: the string to count
  *    * Return: length of the string
- *     */
+*/
 int _strlen_recursion(char *s)
 {
 	if (*s)
